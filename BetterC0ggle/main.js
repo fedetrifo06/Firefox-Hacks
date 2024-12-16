69fc5598bb1dbbeeb7cc030a04435fd5aacf573e
fedetrifo06 <61948220+fedetrifo06@users.noreply.github.com>
2024-12-16 20:11:48 +0100

    Enable premium lines & shapes

--- BetterC0ggle/main.js
+++ BetterC0ggle/main.js
@@ -1,27 +1,72 @@
 (function () {
-    const targetId = "colour-vivid";
-    const newClass = "enabled";
+    const colorsId = "colour-vivid";
+    const shapesClass = "shapemenu";
+    const colorNewClass = "enabled";
+    const containerOldClass = "disabled";
+    const linesId1 = "line_style";
+    const linesId2 = "line_shape";
+    const linesId3 = "line_weight";
   
-    function addClassToChildElements(container) {
-        container.querySelectorAll("*").forEach((element) => {
-          element.classList.add(newClass);
-        });
-      }
+    function colorEnabler() {
+        const container = document.getElementById(colorsId);
+        if (container) {
+            container.querySelectorAll("*").forEach((element) => {
+                element.classList.add(colorNewClass);
+            });
+        }
+    }
+
+    function shapeEnabler() {
+        const container = document.getElementsByClassName(shapesClass)[0];
+        if (container) {
+            container.querySelectorAll("*").forEach((element) => {
+                if (element.classList.contains(containerOldClass))
+                    element.classList.remove(containerOldClass);
+            });
+        }
+    }
+
+    function linesEnabler() {
+        const container1 = document.getElementById(linesId1);
+        if (container1) {
+            container1.querySelectorAll("*").forEach((element) => {
+                element.querySelectorAll("*").forEach((element2) => {
+                    element2.classList.add(colorNewClass);
+                });
+            });
+        }
+
+        const container2 = document.getElementById(linesId2);
+        if (container2) {
+            container2.querySelectorAll("*").forEach((element) => {
+                element.querySelectorAll("*").forEach((element2) => {
+                    element2.classList.add(colorNewClass);
+                });
+            });
+        }
+
+        const container3 = document.getElementById(linesId3);
+        if (container3) {
+            container3.querySelectorAll("*").forEach((element) => {
+                element.querySelectorAll("*").forEach((element2) => {
+                    element2.classList.add(colorNewClass);
+                });
+            });
+        }
+    }
     
-      function observeAndAddClass() {
+    function observeAndAddClass() {
         const observer = new MutationObserver(() => {
-          const container = document.getElementById(targetId);
-          if (container) {
-            addClassToChildElements(container);
-          }
+          colorEnabler();
+          shapeEnabler();
+          linesEnabler();
         });
     
         observer.observe(document.body, { childList: true, subtree: true });
     
-        const container = document.getElementById(targetId);
-        if (container) {
-          addClassToChildElements(container);
-        }
+        colorEnabler();
+        shapeEnabler();
+        linesEnabler();
     }
     
     observeAndAddClass();
